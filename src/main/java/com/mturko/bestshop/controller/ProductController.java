ff8bdb48c6802e1576f28ce2807469280616bc40
maxturko <illu_sion@ukr.net>
2020-12-24 15:01:32 +0200

    renamed ProductsController

--- src/main/java/com/mturko/bestshop/controller/ProductController.java
+++ src/main/java/com/mturko/bestshop/controller/ProductController.java
@@ -4,22 +4,17 @@ import com.mturko.bestshop.model.Product;
 import com.mturko.bestshop.model.dto.ProductDto;
 import com.mturko.bestshop.service.ProductService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.client.ClientHttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "products")
-public class ProductsController {
+public class ProductController {
 
     private final ProductService productService;
 
-    public ProductsController(ProductService productService) {
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
